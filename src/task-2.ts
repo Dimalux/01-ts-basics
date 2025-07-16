47826f4c6a33af7ac1de297fa4b3a3be2771892f
Dima_V <astrallux777@ukr.net>
2025-07-16 18:44:51 +0300

    16.07.2025_TS-06

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,14 +1,15 @@
-interface User {
+interface Product {
   readonly id: number;
   title: string;
   description?: string;
 }
 
-const product: User = {
+const product: Product = {
   id: 1,
   title: "Tablet",
   description: "Compact and fast",
 };
 
 console.log(`Product: ${JSON.stringify(product)}`);
+
 
--- src/task-3.ts
+++ src/task-3.ts
@@ -2,12 +2,12 @@ const usernames: string[] = ["alice", "bob", "charlie"];
 
 const ratings: number[] = [4.5, 3.8, 5];
 
-interface User {
+interface Product {
   id: number;
   title: string;
 }
 
-const products: User[] = [
+const products: Product[] = [
   { id: 1, title: "Phone" },
   { id: 2, title: "Laptop" },
 ];
@@ -15,4 +15,3 @@ const products: User[] = [
 console.log(`Usernames: ${JSON.stringify(usernames)}`);
 console.log(`Ratings: ${JSON.stringify(ratings)}`);
 console.log(`Products: ${JSON.stringify(products)}`);
-
